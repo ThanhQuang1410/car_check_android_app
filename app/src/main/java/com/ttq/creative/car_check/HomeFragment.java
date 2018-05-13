015b4fbcfaa6c5bdf081055930fb408908a7d30d
ThanhQuang1410 <ttquang1410@gmail.com>
2018-05-13 17:44:48 +0700

    loi o gridViewAdapter

--- app/src/main/java/com/ttq/creative/car_check/HomeFragment.java
+++ app/src/main/java/com/ttq/creative/car_check/HomeFragment.java
@@ -6,24 +6,41 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class HomeFragment extends Fragment {
-
-
+    GridView gridView;
+    String[] name = {
+        "toyota","mescerdes","cadilac","chevrolet","astomartin","audi","bmw","ferrari",
+    };
+    int[] logo = {
+        R.drawable.brand_toyota,
+            R.drawable.brand_mescerdes,
+            R.drawable.brand_cadilac,
+            R.drawable.brand_chevrolet,
+            R.drawable.brand_astonmartin,
+            R.drawable.brand_audi,
+            R.drawable.brand_bmw,
+            R.drawable.brand_ferrari,
+    };
     public HomeFragment() {
         // Required empty public constructor
+
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+        gridView =(GridView) view.findViewById(R.id.brand_list);
+        gridViewAdapter gridViewAdapter = new gridViewAdapter(this,name,logo);
+        gridView.setAdapter(gridViewAdapter);
+
+        return view;
     }
 
 }
---
+++ app/src/main/java/com/ttq/creative/car_check/gridViewAdapter.java
@@ -0,0 +1,50 @@
+package com.ttq.creative.car_check;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class gridViewAdapter extends BaseAdapter {
+    private Context context;
+    private String[] brandName;
+    private int[] brandLogo;
+
+    public gridViewAdapter(HomeFragment context, String[] brandName, int[] brandLogo) {
+        this.context = context;
+        this.brandName = brandName;
+        this.brandLogo = brandLogo;
+    }
+
+
+    @Override
+    public int getCount() {
+        return brandName.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater layoutInflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView  = layoutInflater.inflate(R.layout.list_item_layout,null);
+        TextView textView = (TextView)convertView.findViewById(R.id.brand_name);
+        ImageView imageView = (ImageView)convertView.findViewById(R.id.logo_brand);
+
+        textView.setText(brandName[position]);
+        imageView.setImageResource(brandLogo[position]);
+
+        return convertView;
+    }
+}
