de7ea5cd0b2b7f705cad0b54d9fc3bcaa1737c2c
ThanhQuang1410 <ttquang1410@gmail.com>
2018-06-15 16:07:46 +0700

    update

--- app/src/main/java/com/ttq/creative/car_check/HomeFragment.java
+++ app/src/main/java/com/ttq/creative/car_check/HomeFragment.java
@@ -36,11 +36,12 @@ public class HomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_home, container, false);
-        gridView =(GridView) view.findViewById(R.id.brand_list);
-        gridViewAdapter gridViewAdapter = new gridViewAdapter(getContext(),name,logo);
-        gridView.setAdapter(gridViewAdapter);
 
+//        gridView =(GridView) view.findViewById(R.id.brand_list);
+//        gridViewAdapter gridViewAdapter = new gridViewAdapter(getContext(),name,logo);
+//        gridView.setAdapter(gridViewAdapter);
         return view;
+
     }
 
 }
