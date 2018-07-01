78e8d22f71f7a59ced934a20635ccae6d167c196
ThanhQuang1410 <ttquang1410@gmail.com>
2018-07-01 10:16:30 +0700

    ADD Fragment news_list và Fragment featured_list

--- app/src/main/java/com/ttq/creative/car_check/main.java
+++ app/src/main/java/com/ttq/creative/car_check/main.java
@@ -24,6 +24,7 @@ public class main extends AppCompatActivity {
     private FollowFragment followFragment;
     private MoreFragment moreFragment;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
